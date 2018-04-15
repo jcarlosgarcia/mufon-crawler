84acde66cb4137626997b5d559dc393f97986fe0
José Carlos García <jcarlosgarcia@hotmail.com>
2018-04-15 20:15:46 +0200

    Error handling and included any term to perform the report selection

--- mufon-crawler.py
+++ mufon-crawler.py
@@ -10,7 +10,7 @@ import sys
 import time
 
 BASE_URL_BY_ID = "http://www.ufostalker.com:8080/event?id="
-BASE_URL_BY_COUNTRY = "http://ufostalker.com:8080/search?type=all&size=10&term="
+BASE_URL_BY_TERM = "http://ufostalker.com:8080/search?type=all&size=10&term="
 SOURCE = "MUFON"
 REPORTS_BY_PAGE = 10
 TIME_DELAY = 5
@@ -37,10 +37,10 @@ class Sighting:
     return [self.id, self.sighted_at, self.reported_at, self.location, self.shape, self.duration, self.description, self.latitude, self.longitude, self.case_number]
 
 
-def check_arguments(initial, end, country, output):
-  if country is None:
+def check_arguments(initial, end, term, output):
+  if term is None:
     if initial is None or end is None:
-      print("'initial' and 'end' must be passed unless you specify a country")
+      print("'initial' and 'end' must be passed unless you specify a term")
       return False
     if initial > end:
       print("'initial' must be less than or equal to 'end'")
@@ -50,7 +50,7 @@ def check_arguments(initial, end, country, output):
     print("'output' must be specified")
     return False
 
-  # If country is passed, 'initial' and 'end' are ignored
+  # If term is passed, 'initial' and 'end' are ignored
   return True
 
 
@@ -65,6 +65,11 @@ def geolocate(city, country):
 
 def parse_report(base_url, x):
   source = requests.get("%s%s" % (base_url, x), headers = {'User-Agent': 'mufon-crawler'}, stream = True)
+
+  if source.status_code is not 200:
+    print('Did not get a status OK, %s', source.status_code)
+    sys.exit()
+
   source.raw.decode_content = True
 
   doc = etree.parse(source.raw)
@@ -90,23 +95,32 @@ def parse_report(base_url, x):
 
   return report
 
-def parse_reports_by_country(base_url, country, page_number):
-  source = requests.get("%s%s&page=%s" % (base_url, country, page_number), headers = {'User-Agent': 'mufon-crawler'})
-  #source.raw.decode_content = True
+def parse_reports_by_term(base_url, term, page_number):
+  source = requests.get("%s%s&page=%s" % (base_url, term, page_number), headers = {'User-Agent': 'mufon-crawler'})
 
-  print(source.content)
+  if source.status_code is not 200:
+    print('Did not get a status OK, %s', source.status_code)
+    sys.exit()
 
   doc = json.loads(source.text)
   content = doc['content']
-
-  print(content)
 
   reports = []
 
   for report in content:
     id = report['id']
-    reported_at = datetime.fromtimestamp(int(report['submitted']) / 1000.0).strftime('%Y-%m-%d %H:%M:%S')
-    sighted_at = datetime.fromtimestamp(int(report['occurred']) / 1000.0).strftime('%Y-%m-%d %H:%M:%S')
+    reported_at = None
+    try:
+      reported_at = datetime.fromtimestamp(int(report['submitted']) / 1000.0).strftime('%Y-%m-%d %H:%M:%S')
+    except:
+      print('Could not generate a valid report date')
+
+    sighted_at = None
+    try:
+      sighted_at = datetime.fromtimestamp(int(report['occurred']) / 1000.0).strftime('%Y-%m-%d %H:%M:%S')
+    except:
+      print('Could not generate a valid sighting date')
+
     location = "%s (%s)" % (report['city'], report['country'])
     shape = report['shape']
     duration = report['duration']
@@ -116,9 +130,14 @@ def parse_reports_by_country(base_url, country, page_number):
     case_number = report['logNumber']
 
     if latitude is None or longitude is None:
-      loc = geolocate(report['city'], report['country'])
-      latitude = loc.latitude
-      longitude = loc.longitude
+      try:
+        loc = geolocate(report['city'], report['country'])
+        latitude = loc.latitude
+        longitude = loc.longitude
+      except:
+        print('Could not geolocate the sighting')
+        latitude = None
+        longitude = None
 
     sighting = Sighting(id, sighted_at, reported_at, location, shape, duration, description, latitude, longitude, case_number)
 
@@ -130,8 +149,8 @@ def parse_reports_by_country(base_url, country, page_number):
 parser = argparse.ArgumentParser(description = 'Generates a CSV from MUFON reports')
 parser.add_argument('-i', '--initial', type = int, help = 'Initial report id')
 parser.add_argument('-e', '--end', type = int, help = 'Final report id')
-parser.add_argument('-c', '--country', type = str, help = 'Only reports from this country')
-parser.add_argument('-l', '--limit', default = 10, type = int, help = 'Max number of reports by country')
+parser.add_argument('-t', '--term', type = str, help = 'Only reports including this term, e.g., country, shape, etc.')
+parser.add_argument('-l', '--limit', default = 10, type = int, help = 'Max number of reports by term')
 parser.add_argument('-o', '--output', type = str, help = 'CSV file name')
 
 args = parser.parse_args()
@@ -139,17 +158,17 @@ args = parser.parse_args()
 begin_index = args.initial
 end_index = args.end
 out_file = args.output
-country = args.country
+term = args.term
 limit = args.limit
 
-if not check_arguments(begin_index, end_index, country, out_file):
+if not check_arguments(begin_index, end_index, term, out_file):
   sys.exit()
 
 header = ["id", "sighted_at", "reported_at", "location", "shape", "duration", "description", "latitude", "longitude", "case_number"]
 out = csv.writer(open(out_file, "w"), delimiter = ',', quoting = csv.QUOTE_ALL)
 out.writerow(header)
 
-if country is None:
+if term is None:
   for x in range(int(begin_index), int(end_index) + 1):
     print("Downloading reports by report number... %s%s" % (BASE_URL_BY_ID, x))
 
@@ -157,7 +176,7 @@ if country is None:
     out.writerow(report.to_array())
     time.sleep(TIME_DELAY)
 else:
-  print("Downloading reports by country... %s%s" % (BASE_URL_BY_COUNTRY, country))
+  print("Downloading reports by term... %s%s" % (BASE_URL_BY_TERM, term))
   n_pages = limit // REPORTS_BY_PAGE
   if limit % REPORTS_BY_PAGE is not 0:
     n_pages += 1
@@ -165,7 +184,7 @@ else:
   total_reports = 0
 
   for x in range(1, n_pages + 1):
-    reports = parse_reports_by_country(BASE_URL_BY_COUNTRY, country, x)
+    reports = parse_reports_by_term(BASE_URL_BY_TERM, term, x)
     for report in reports:
       out.writerow(report.to_array())
       total_reports += 1
